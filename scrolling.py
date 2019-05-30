506e049dc35260b8e4ca09d732af59c02d15fa7e
Srihari Vishnu <srihari.vishnu@gmail.com>
2019-05-30 18:58:39 -0400

    Update scrolling.py

--- scrolling.py
+++ scrolling.py
@@ -26,7 +26,7 @@ def get_light(center, angle, walls):
         ydisp = (targetposy - center[1]) / MAX_DISTANCE
         for y in range(MAX_DISTANCE):
             for wall in walls_sprites:
-                if wall.rect.collidepoint((center[0] + xdisp * y), (center[1] + ydisp * y)):
+                if camera.apply(wall).collidepoint((center[0] + xdisp * y), (center[1] + ydisp * y)):
                     pointlist.append(((center[0] + xdisp * y), (center[1] + ydisp * y)))
                     hit = True
                     break
@@ -43,16 +43,18 @@ def check_collisions():
     new_rect = pygame.Rect(position[0] + player.xvel - player.rect[2] / 2,
                            position[1] + player.yvel - player.rect[3] / 2, player.rect[2], player.rect[3])
     for wall in walls_sprites:
-        if new_rect.colliderect(wall.rect):
+        if new_rect.colliderect(wall):
             collides = True
 
     if not collides:
         player.position[0] += player.xvel
+        #camera.camera.x -= player.xvel
     else:
         player.bouncex()
 
     if not collides:
         player.position[1] += player.yvel
+        #camera.camera.y -= player.yvel
     else:
         player.bouncey()
 
@@ -64,19 +66,20 @@ def draw_screen():
     screen.blit(box_surface_fill, (0, 0))
     sprites.update()
     walls_sprites.update()
-    for sprite in sprites:
-        sprite.rect = camera.apply(sprite)
-    for wall in walls_sprites:
-        wall.rect = camera.apply(wall)
-        print (camera.apply(wall))
-    walls_sprites.draw(screen)
-    pygame.display.flip()
     camera.update(player, WIDTH, HEIGHT)
+    for sprite in sprites:
+        screen.blit(sprite.image, camera.apply(sprite))
 
-# WIDTH = 1250
-# HEIGHT = 950
-WIDTH = 500
-HEIGHT = 500
+    for wall in walls_sprites:
+        screen.blit(wall.image, camera.apply(wall))
+
+    pygame.display.flip()
+
+
+WIDTH = 1250
+HEIGHT = 950
+#WIDTH = 500
+#HEIGHT = 500
 pygame.init()
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
@@ -91,12 +94,12 @@ walls_sprites = pygame.sprite.Group()
 for y, tiles in enumerate(map.map_data):
     for x, tile in enumerate(tiles):
         if tile == "#":
-            Wall(x,y,32, 32, walls_sprites)
+            Wall(x*32, y*32, 32, 32, walls_sprites)
 
-player = Player([400, 300], 20)
+player = Player([WIDTH/2, HEIGHT/2], 20)
 sprites.add(player)
 
-camera = Camera(map.width, map.height)
+camera = Camera(WIDTH, HEIGHT)
 targetangle = 260
 crashed = False
 forward = False
@@ -145,7 +148,7 @@ while not crashed:
     check_collisions()
     player.set_position(position[0], position[1])
 
-    new_angle = calculate_angle(mouse_position[0], mouse_position[1], WIDTH//2, HEIGHT//2)
+    new_angle = calculate_angle(mouse_position[0], mouse_position[1], WIDTH/2 + player.width, HEIGHT/2 + player.width)
     if new_angle:
         targetangle = new_angle
 
