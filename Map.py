beb7fe81f418460b9f6db3ed9ea4b1cc130b9d31
Srihari Vishnu <srihari.vishnu@gmail.com>
2019-05-30 20:57:51 -0400

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,15 +1,13 @@
 import pygame
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, pcx, pcy, width, height, group):
+    def __init__(self, x,y, width, height, group):
         self.groups = group
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.image = pygame.Surface((width,height))
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
-        self.pcx = pcx
-        self.pcy = pcy
-        self.rect.x = pcx*width
-        self.rect.y = pcy*height
+        self.rect.x = x
+        self.rect.y = y
 
 class Map:
     def __init__(self):
@@ -18,22 +16,23 @@ class Map:
             for line in file:
                 self.map_data.append(line)
 
-        self.mapwidth = len(self.map_data[0])
-        self.mapheight = len(self.map_data)
-
-        self.width = self.mapwidth * 32
-        self.height = self.mapheight * 32
+        self.width = len(self.map_data[0]) * 32
+        self.height = len(self.map_data) * 32
 
 class Camera:
     def __init__(self, width, height):
-        self.camera = pygame.Rect(0,0, width, height)
+        self.camera = pygame.Rect(0,0, width*32, height*32)
         self.width = width
         self.height = height
 
     def apply(self, sprite):
-        return sprite.rect.move(self.camera.topleft)
+        new_rect = pygame.Rect(sprite.rect.x + self.camera.x, sprite.rect.y + self.camera.y, sprite.rect.width, sprite.rect.height)
+        return new_rect
 
     def update(self, target, screenw, screenh):
         x = -target.rect.x + screenw //2
         y = -target.rect.y + screenh // 2
+
+        x = max(min(0,x), - (self.width + 32))
+        y = max(min(0,y), -(self.height + 32))
         self.camera = pygame.Rect(x,y, self.width, self.height)
